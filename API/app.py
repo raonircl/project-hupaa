1d7e535c26efc0318b524e429915832d8028ec6d
raonircl <raoni.afi@gmail.com>
2024-08-24 02:57:48 -0300

    import routes

--- API/app.py
+++ API/app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from routes import hello, users
 
 app = FastAPI()
+
+app.include_router(hello.router, prefix="/api/v1")
+app.include_router(users.router, prefix="/api/v1")
 
 @app.get("/api/v1/")
 async def say_hello():
