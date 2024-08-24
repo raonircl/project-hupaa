50dcfc5380ce5dd6ddaab36ad7013199836fedee
raonircl <raoni.afi@gmail.com>
2024-08-24 03:15:58 -0300

    fix: DATABASE URL

--- API/config/database.py
+++ API/config/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
